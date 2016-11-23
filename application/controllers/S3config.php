11a966c47904ad1d02b61223022a66f242112515
trendyapplication <trendyapplication@users.noreply.github.com>
2016-11-23 11:45:11 -0600

    Update S3config.php

--- application/controllers/S3config.php
+++ application/controllers/S3config.php
@@ -1,7 +1,7 @@
 <?php
 
-$trendy_AccessKey = "AKIAII3H274RKDNE4SNQ";
-$trendy_SecretKey = "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";
+$trendy_AccessKey = "";
+$trendy_SecretKey = "";
 
 define("S3_ACCESS_KEY",$trendy_AccessKey);
 define("S3_SECRET_KEY",$trendy_SecretKey);
